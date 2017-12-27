5015d58b7af270ac6e682a28d0518c8bd5613d30
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2017-12-27 13:04:28 -0500

    make it better

--- routes.js
+++ routes.js
@@ -19,7 +19,7 @@ router.get('/movie/:query', (req, res) => {
     }         
   })
   .then((results) => {
-    // res.set('Content-Type', 'application/json');
+    res.set('Access-Control-Allow-Origin', '*');
     res.json(results.data)
   })
   .catch(err => console.error(err))
@@ -50,8 +50,8 @@ router.get('/showtimes/:id/:lat/:lng/:radius/:movie', (req, res) => {
   .then((results) => {
     let showtimes = results.data.showtimes;
     util.cinemaLocation(showtimes, lat, lng, radius, movie, (showtimesWithLoc) => {
-      // res.set('Content-Type', 'application/json');
-      res.json(showtimesWithLoc);
+      res.set('Access-Control-Allow-Origin', '*');
+      res.json(showtimesWithLoc || [{}]);
     })
   })
   .catch(err => console.error(err));
