cce3607d3efa4c0e78d1c61a5cae8a3829f248fc
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2017-12-26 23:42:00 -0500

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/www'));
 app.use('/search', router);
 
 
-const server = app.listen(process.env.PORT || 8080, '127.0.0.1', function() {
+const server = app.listen(process.env.PORT, '127.0.0.1', function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`Google Map Practice app listening at http://${host}:${port}`);
